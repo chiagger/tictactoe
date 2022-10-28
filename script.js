c5d80d2a9cdd82e09bd3eb4ef1e80f97e20997e1
chiagger <busca.chiara.cb@gmail.com>
2022-10-28 18:00:58 +0200

    fixed wrong rules bug

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const restart = document.getElementById("restart");
 const resultContainer = document.getElementById("result");
 
 
+
 const GameBoard = (
     () => {
         let gameBoard = Array.apply(null, Array(9)).map(function () { });
@@ -23,33 +24,46 @@ const GameBoard = (
             return symbol;
         }
 
+        function getCurrentPlayerSymbol() {
+            let symbol;
+            if (player === 0) {
+                symbol = "X";
+            } else if (player === 1) {
+                symbol = "O";
+            } else {
+                alert("Error");
+            }
+            return symbol;
+        }
+
         function winMessage(nr) {
-            rules.style.visibility = "hidden";
+            while (rules.hasChildNodes()) {
+                rules.removeChild(rules.firstChild);
+            }
             const result = document.createElement('div');
             result.textContent = "Player \"" + playerSymbol(nr) + "\" wins!";
             if (resultContainer.hasChildNodes()) {
                 resultContainer.removeChild(resultContainer.firstChild);
-            } 
+            }
             resultContainer.appendChild(result);
             restart.style.visibility = "visible";
         }
 
-        const whoStarts = document.getElementById("rules");
-        whoStarts.textContent = "Player \"" + playerSymbol(player) + "\" starts.";
-
         function oppositePlayer(nr) {
-            let player;
+            let res;
             if (nr === 0) {
-                player = 1;
+                res = 1;
             } else if (nr === 1) {
-                player = 0;
+                res = 0;
             } else {
                 alert("Error");
             }
-            return player;
+            return res;
         }
 
         const move = (index) => {
+
+
             if (gameBoard.at(index) === 1 ||
                 gameBoard.at(index) === 0) {
                 //Can't change spot that's already taken
@@ -69,44 +83,44 @@ const GameBoard = (
         const checkWin = () => {
             let won = false;
             for (let i = 0; i < 9; i += 3) {
-                if (won===false && gameBoard.at(i) === gameBoard.at(i + 1) &&
+                if (won === false && gameBoard.at(i) === gameBoard.at(i + 1) &&
                     gameBoard.at(i) === gameBoard.at(i + 2) &&
                     gameBoard.at(i) != null &&
                     gameBoard.at(i + 1) != null &&
                     gameBoard.at(i + 2) != null) {
                     resultContainer.style.visibility = "visible";
-                    won=true;
+                    won = true;
                     winMessage(gameBoard.at(i));
 
                 }
             }
             for (let i = 0; i < 3; i++) {
-                if (won===false && gameBoard.at(i) === gameBoard.at(i + 3) &&
+                if (won === false && gameBoard.at(i) === gameBoard.at(i + 3) &&
                     gameBoard.at(i) === gameBoard.at(i + 6) &&
                     gameBoard.at(i) != null &&
                     gameBoard.at(i + 3) != null &&
                     gameBoard.at(i + 6) != null) {
                     resultContainer.style.visibility = "visible";
-                    won=true;
+                    won = true;
                     winMessage(gameBoard.at(i));
                 }
             }
-            if (won===false && gameBoard.at(0) === gameBoard.at(4) &&
+            if (won === false && gameBoard.at(0) === gameBoard.at(4) &&
                 gameBoard.at(0) === gameBoard.at(8) &&
                 gameBoard.at(0) != null &&
                 gameBoard.at(4) != null &&
                 gameBoard.at(8) != null) {
                 resultContainer.style.visibility = "visible";
-                won=true;
+                won = true;
                 winMessage(gameBoard.at(0));
             }
-            if (won===false && gameBoard.at(2) === gameBoard.at(4) &&
+            if (won === false && gameBoard.at(2) === gameBoard.at(4) &&
                 gameBoard.at(2) === gameBoard.at(6) &&
                 gameBoard.at(2) != null &&
                 gameBoard.at(4) != null &&
                 gameBoard.at(6) != null) {
                 resultContainer.style.visibility = "visible";
-                won=true;
+                won = true;
                 winMessage(gameBoard.at(2));
             }
             if (won === false && gameBoard.at(0) != null &&
@@ -118,18 +132,20 @@ const GameBoard = (
                 gameBoard.at(6) != null &&
                 gameBoard.at(7) != null &&
                 gameBoard.at(8) != null) {
-                rules.style.visibility = "hidden";
+                while (rules.hasChildNodes()) {
+                    rules.removeChild(rules.firstChild);
+                }
                 const result = document.createElement('div');
                 result.textContent = "That's a Tie!";
                 if (resultContainer.hasChildNodes()) {
                     resultContainer.removeChild(resultContainer.firstChild);
-                } 
+                }
                 resultContainer.appendChild(result);
                 resultContainer.style.visibility = "visible";
                 restart.style.visibility = "visible";
             }
         }
-        return { move, checkWin, reset };
+        return { move, checkWin, reset, getCurrentPlayerSymbol };
     }
 )();
 
@@ -141,28 +157,29 @@ function game() {
 }
 
 restart.style.visibility = "hidden";
-rules.style.visibility = "hidden";
 startBtn.addEventListener("click", () => {
     game();
     startBtn.style.visibility = "hidden";
-    rules.style.visibility = "visible";
+    const rulesContent = document.createElement("div");
+    rulesContent.textContent = "Player \"" + GameBoard.getCurrentPlayerSymbol() + "\" starts.";
+    rules.appendChild(rulesContent);
 }
 );
 
 restart.addEventListener("click", () => {
     while (resultContainer.hasChildNodes()) {
         resultContainer.removeChild(resultContainer.firstChild);
-
     }
+
     restart.style.visibility = "hidden";
-    rules.style.visibility = "hidden";
     cells.forEach(cell => {
         cell.textContent = "";
     });
     GameBoard.reset();
     game();
-    startBtn.style.visibility = "hidden";
-    rules.style.visibility = "visible";
+    const rulesContent = document.createElement("div");
+    rulesContent.textContent = "Player \"" + GameBoard.getCurrentPlayerSymbol() + "\" starts.";
+    rules.appendChild(rulesContent);
 });
 
 
